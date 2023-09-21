5bd0b07acc6743c1de950b95d94bdc645c1e7d69
Gabriel Pavão <gabpavao@outlook.com>
2023-09-21 12:38:36 -0300

    feat(Navbar): create and style the Navbar component

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,17 @@
+import calendarIcon from '@/assets/icons/calendar.svg'
+import chatIcon from '@/assets/icons/chat.svg'
+import creditArrowIcon from '@/assets/icons/credit-arrow.svg'
+import debitArrowIcon from '@/assets/icons/debit-arrow.svg'
+import documentIcon from '@/assets/icons/document.svg'
+import menuIcon from '@/assets/icons/menu.svg'
+import speakerIcon from '@/assets/icons/speaker.svg'
+
+export {
+	calendarIcon,
+	chatIcon,
+	creditArrowIcon,
+	debitArrowIcon,
+	documentIcon,
+	menuIcon,
+	speakerIcon
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import dcLogo from '@/assets/images/dc-light-logo.svg'
-import { List } from '@phosphor-icons/react'
+import { menuIcon } from '@/assets/icons'
 
 export default function Header() {
 	return (
-		<header className='bg-primary w-screen flex items-center justify-between p-4 rounded-b-md sm:rounded-none'>
+		<header className='bg-primary w-full flex items-center justify-between p-4 rounded-b-md sm:rounded-none relative'>
 			<div className='flex items-center'>
 				<Image src={dcLogo} alt='Logo da Delta Code' width={50} height={50}/>
 				<h1 className='text-white text-left text-base tracking-wide font-medium ml-4 sm:ml-8'>Tela Inicial</h1>
 			</div>
 			<button type='button' className='h-11 w-11 bg-primary-light rounded-full grid place-items-center transition hover:bg-primary-dark'>
-				<List size={28} color='white'/>
+				<Image src={menuIcon} alt='Menu Dropdown' width={25} height={25}/>
 			</button>
 		</header>
 	)
