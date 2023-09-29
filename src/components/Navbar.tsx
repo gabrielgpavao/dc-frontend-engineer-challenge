128b378b1a46a8e1b12857ed2cf0d40bcc6578c9
Gabriel Pavão <gabpavao@outlook.com>
2023-09-29 14:55:25 -0300

    style: fix positioning in the Header and Navbar components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,18 @@ import { calendarIcon, documentIcon, speakerIcon, chatIcon } from '@/assets/icon
 
 function NavItem ({ imageSrc, title }: { imageSrc: StaticImageData, title: string }) {
 	return (
-		<li className='flex flex-col items-center cursor-pointer'>
-			<figure className='w-6 h-6'>
-				<Image src={imageSrc} alt={title} title={title} className='h-full w-full'/>
+		<li className='flex flex-col items-center gap-[0.375rem] cursor-pointer'>
+			<figure className='flex justify-center md:h-10 md:items-center'>
+				<Image src={imageSrc} alt={title} title={title} className='h-6 w-6'/>
 			</figure>
-			<h3 className='mt-1 text-sm text-[#ffffffda] font-light md:hidden'>{title}</h3>
+			<h3 className='text-[0.8125rem] text-[#FFFFFFBF] font-light md:hidden'>{title}</h3>
 		</li>
 	)
 }
 
 export default function Navbar() {
 	return (
-		<nav className='p-4 bg-primary rounded-t-md sm:rounded-none navbarMd'>
+		<nav className='h-[4.6875rem] px-5 flex justify-center bg-primary rounded-t-md sm:rounded-none navbarMd'>
 			<ul className='flex justify-between items-center w-full ulNavbarMd'>
 				<NavItem imageSrc={calendarIcon} title='Calendário'/>
 				<NavItem imageSrc={speakerIcon} title='Comunicados'/>
